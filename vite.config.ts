f417673e12c802d55b7564cb009f08664f4f72ef
Matthias <m@tth.es>
2025-08-07 16:32:39 +0200

    sync

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,19 @@ export default defineConfig({
   root: './',
   publicDir: './public/www',
   build: {
-    rollupOptions: {
-      input: './public/main.ts'
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: "tj-alpha",
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es' as const],
     }
   }
 });
